2dba221a54ff41c6b320355eb84b410a68edefaf
Maciej Krajewski <mkrajewski@yameo.eu>
2022-06-20 19:43:27 +0200

    changes in the script

--- 01_BASH/solution/backup.sh
+++ 01_BASH/solution/backup.sh
@@ -9,19 +9,22 @@ function print () {
 }
 function install () {
     apt-get update
-    apt-get install --no-install-recommends -y python3 redis python3-pip
+    apt-get install --no-install-recommends -y python3 redis python3-pip uvicorn
     cd /root/DEVOPS/01_BASH/Python_app/
     pip3 install --no-cache-dir -r requirements.txt
     echo "[Unit]
-    Description=Python API
-    After=network.target
-    [Service]
-    Type=simple
-    Environment=REDIS_HOST=127.0.0.1
-    ExecStart=/usr/bin/python3 /usr/bin/main.py
-    StandardInput=tty-force
-    [Install]
-    WantedBy=multi-user.target" > /lib/systemd/system/python-api.service
+Description=Python API
+After=network.target
+
+[Service]
+WorkingDirectory=/root/DEVOPS/01_BASH/Python_app
+Type=simple
+Environment=REDIS_HOST=127.0.0.1
+ExecStart=/usr/bin/uvicorn main:app --host 0.0.0.0 --port 5002
+StandardInput=tty-force
+
+[Install]
+WantedBy=multi-user.target" > /lib/systemd/system/python-api.service
     systemctl daemon-reload
     systemctl enable python-api.service
     systemctl start python-api.service
