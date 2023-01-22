9eb424dae5396abac54d466030c674e1fbce8ea4
Maciej Krajewski <mkrajewski@yameo.eu>
2023-01-22 20:08:12 +0100

    changes in repo

--- 01_BASH/Python_app/main.py
+++ 01_BASH/Python_app/main.py
@@ -3,15 +3,44 @@ import redis
 import socket
 import logging
 import os
-import logging
 import time
 
+LOG_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": {"default": {"format": "%(asctime)s [%(process)s] %(levelname)s: %(message)s"}},
+    "handlers": {
+        "console": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "level": "INFO",
+        },
+        "console": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "level": "DEBUG",
+        }
+    },
+    "root": {"handlers": ["console"], "level": "INFO"},
+    "loggers": {
+        "uvicorn": {"propagate": True},
+        "uvicorn.access": {"propagate": True},
+        "uvicorn.error": {"propagate": True},
+    },
+}
+
+logging.config.dictConfig(LOG_CONFIG)
+
 LOGGER = logging.getLogger("uvicorn.info")
+
 level = os.environ.get("LOG_LEVEL","INFO")
 if level == "INFO":
     LOGGER.setLevel(logging.INFO)
 if level == "DEBUG":
     LOGGER.setLevel(logging.DEBUG)
+
 
 app = FastAPI()
 hostname=socket.gethostname()
@@ -35,9 +64,9 @@ async def healthz():
 
 @app.get("/api/v1/info")
 async def info():
+    started_at = time.time()
     r = get_redis()
     counter =  r.get('counter')
-    started_at = time.time()
     if counter is None:
         counter = 0
     else:
@@ -49,10 +78,13 @@ async def info():
 
 @app.post("/api/v1/info")
 def info_post():
+    started_at = time.time()
     r = get_redis()
     previous = r.get('counter')
     if previous is None:
         r.set('counter', 1)
     else:
         r.incr('counter')
+    duration = time.time() - started_at
+    LOGGER.debug('Request took %s', duration)
     return {"message": "OK", "hostname": hostname}
